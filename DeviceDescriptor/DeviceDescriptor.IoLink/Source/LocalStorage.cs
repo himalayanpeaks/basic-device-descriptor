4d3b38dad0b61b839d523a06d105899132873c8d
Rahul <83376754+himalayanpeaks@users.noreply.github.com>
2025-05-27 00:00:39 +0200

    Adding subindex, parent id working ok

--- DeviceDescriptor/DeviceDescriptor.IoLink/Source/LocalStorage.cs
+++ DeviceDescriptor/DeviceDescriptor.IoLink/Source/LocalStorage.cs
@@ -1,11 +1,10 @@
 ﻿using DeviceDescriptor.Abstract;
 using DeviceDescriptor.IoLink.Variables;
-using DeviceDescriptor.IoLink.IODD1_1; // Namespace from your IODD1_1.cs
+using DeviceDescriptor.IoLink.IODD1_1;
 using System.Xml.Serialization;
 using DeviceDescriptor.Abstract.Variables;
 using static DeviceDescriptor.Abstract.Definition;
 using System.Globalization;
-
 
 namespace DeviceDescriptor.IoLink.Source
 {
@@ -28,32 +27,20 @@ namespace DeviceDescriptor.IoLink.Source
                 return Task.FromResult<BasicDescriptor<Variable>?>(null);
             }
 
-            if (root == null || root.ProfileBody?.DeviceFunction == null)
+            if (root?.ProfileBody?.DeviceFunction == null)
                 return Task.FromResult<BasicDescriptor<Variable>?>(null);
 
-            //Get all avaliable variables from the DeviceFunction
-            var variableDefs = root.ProfileBody.DeviceFunction.VariableCollection?
-                .Variable
-                .OfType<VariableT>()
-                .ToList();
-            //Get all datatype definitions from the DeviceFunction
-            var datatypeMap = root.ProfileBody.DeviceFunction.DatatypeCollection?
-                .Datatype
-                .OfType<DatatypeT>()
-                .ToDictionary(d => d.id, d => d);
+            var variableDefs = root.ProfileBody.DeviceFunction.VariableCollection?.Variable?.OfType<VariableT>().ToList();
+            var datatypeMap = root.ProfileBody.DeviceFunction.DatatypeCollection?.Datatype?.OfType<DatatypeT>().ToDictionary(d => d.id, d => d);
 
             var variables = new List<Variable>();
-            int varCount = 0, dataRefCount = 0;
-            DataType dataType = DataType.CHAR;
-            int lengthinbits = 0; 
-            string? minValue = null, maxValue = null, valid = null, defaultValue = null;
-            int offset = 0;
+
             foreach (var varDef in variableDefs ?? Enumerable.Empty<VariableT>())
             {
-                #region CommonAttributes
-                #region required
                 string name = varDef.id;
                 int index = varDef.index != 0 ? varDef.index : -1;
+                bool isDynamic = varDef.dynamic;
+
                 AccessType access = varDef.accessRights switch
                 {
                     AccessRightsT.ro => AccessType.R,
@@ -61,103 +48,138 @@ namespace DeviceDescriptor.IoLink.Source
                     AccessRightsT.rw => AccessType.RW,
                     _ => AccessType.RW
                 };
-                #endregion
-                #region optional
-                bool isDynamic = varDef.dynamic;
-                #endregion
-                #endregion CommonAttributes
 
+                string? defaultValue = null;
+                if (varDef is VariableCollectionTVariable v && !string.IsNullOrWhiteSpace(v.defaultValue))
+                    defaultValue = v.defaultValue;
 
-                if (varDef.Item is DatatypeT dtDef)
+                if (varDef.Item is DatatypeT inlineDt)
                 {
-                    //Assin type
-                    dataType = dtDef?.GetType().Name switch
-                    {
-                        nameof(UIntegerT) => DataType.UINT,
-                        nameof(IntegerT) => DataType.INT,
-                        nameof(BooleanT) => DataType.BOOL,
-                        nameof(Float32T) => DataType.Float32,
-                        nameof(StringT) => DataType.CHAR,
-                        nameof(RecordT) => DataType.Record,
-                        _ => DataType.Byte
-                    };
-                    varCount++;
-                    if (varDef is VariableCollectionTVariable v && !string.IsNullOrWhiteSpace(v.defaultValue))                    
-                        defaultValue = v.defaultValue;                    
-                    
-                    switch (varDef.Item)
-                    {                        
-                        case UIntegerT u:                            
-                            (minValue, maxValue) = GetMinMaxFromValueRange(u.Items);
-                            valid = GetValidValues(u.Items);
-                            lengthinbits = u.bitLength;
-                            break;
-                        case IntegerT i:
-                            (minValue, maxValue) = GetMinMaxFromValueRange(i.Items);
-                            valid = GetValidValues(i.Items);
-                            lengthinbits = i.bitLength;
-                            break;
-                        case Float32T f:
-                            (minValue, maxValue) = GetMinMaxFromValueRange(f.Items);
-                            valid = GetValidValues(f.Items);
-                            lengthinbits = 32;
-                            break;
-                        case StringT s:
-                            lengthinbits = 8 * s.fixedLength;
-                            break;
-                        case BooleanT b:
-                            valid = GetValidValues(b.SingleValue);                            
-                            lengthinbits = 8;
-                            break;
-                        case RecordT r:
-                            lengthinbits = r.bitLength;
-                            break;
-                        default:
-                            // array type not handles
-                            continue; // Skip this variable definition
-                    }
+                    ParseDatatype(inlineDt, datatypeMap!, variables, name, index, 0, 0, isDynamic, access, defaultValue);
                 }
-                    if(varDef.Item is DatatypeRefT refT)
-                    {
-                        var dtypeId = refT.datatypeId;
-                        dataRefCount++;
-                    }
-
-
-                    var variable = new Variable(
-                    name: name,
-                    index: index,
-                    isDynamic: isDynamic, 
-                    subindex: 0, 
-                    access: access,
-                    dataType: DataType.UINT,
-                    arrayCount: 1, //Can't handle array. Rerely used
-                    lengthInBits: lengthinbits, 
-                    offset: offset, 
-                    defaultValue: defaultValue, 
-                    minimum: minValue, 
-                    maximum: maxValue, 
-                    valid: valid, 
-                    value: null 
-                );
-
-                variables.Add(variable);
+                else if (varDef.Item is DatatypeRefT refT && datatypeMap != null && datatypeMap.TryGetValue(refT.datatypeId, out var dtDef))
+                {
+                    ParseDatatype(dtDef, datatypeMap, variables, name, index, 0, 0, isDynamic, access, defaultValue);
+                }
             }
 
-            var deviceVariables = new DeviceVariables<Variable>
-            {
-                SpecificVariableCollection = variables, //in progress..
-                StandardVariableCollection = new List<Variable>(), 
-                SystemVariableCollection = new List<Variable>(),  
-                CommandCollection = new List<Variable>()          
-            };
-
             var descriptor = new BasicDescriptor<Variable>(
-                variables: deviceVariables,
-                processData: null // Assuming null for ProcessData
+                new DeviceVariables<Variable>
+                {
+                    SpecificVariableCollection = variables,
+                    StandardVariableCollection = new List<Variable>(),
+                    SystemVariableCollection = new List<Variable>(),
+                    CommandCollection = new List<Variable>()
+                },
+                null // processData
             );
 
             return Task.FromResult<BasicDescriptor<Variable>?>(descriptor);
+        }
+
+        private static void ParseDatatype(
+            DatatypeT dt,
+            Dictionary<string, DatatypeT> datatypeMap,
+            List<Variable> variables,
+            string parentId,
+            int index,
+            int subindex,
+            int offset,
+            bool isDynamic,
+            AccessType access,
+            string? defaultValue)
+        {
+            DataType dataType = DataType.Byte;
+            int lengthInBits = 0;
+            string? minValue = null, maxValue = null, valid = null;
+
+            switch (dt)
+            {
+                case UIntegerT u:
+                    dataType = DataType.UINT;
+                    lengthInBits = u.bitLength;
+                    (minValue, maxValue) = GetMinMaxFromValueRange(u.Items);
+                    valid = GetValidValues(u.Items);
+                    break;
+
+                case IntegerT i:
+                    dataType = DataType.INT;
+                    lengthInBits = i.bitLength;
+                    (minValue, maxValue) = GetMinMaxFromValueRange(i.Items);
+                    valid = GetValidValues(i.Items);
+                    break;
+
+                case Float32T f:
+                    dataType = DataType.Float32;
+                    lengthInBits = 32;
+                    (minValue, maxValue) = GetMinMaxFromValueRange(f.Items);
+                    valid = GetValidValues(f.Items);
+                    break;
+
+                case StringT s:
+                    dataType = DataType.CHAR;
+                    lengthInBits = s.fixedLength * 8;
+                    break;
+
+                case BooleanT b:
+                    dataType = DataType.BOOL;
+                    lengthInBits = 8;
+                    valid = GetValidValues(b.SingleValue);
+                    break;
+
+                case RecordT r:
+                    lengthInBits = r.bitLength;
+                    dataType = DataType.Record;
+                    var local = new Variable(
+                                name: parentId,
+                                index: index,
+                                subindex: subindex,
+                                access: access,
+                                isDynamic: isDynamic,
+                                dataType: dataType,
+                                arrayCount: 1,
+                                lengthInBits: lengthInBits,
+                                offset: 0,
+                                defaultValue: defaultValue,
+                                minimum: minValue,
+                                maximum: maxValue,
+                                valid: valid,
+                                value: null
+                            );
+
+                    variables.Add(local);
+                    foreach (var item in r.RecordItem)
+                    {
+                       
+                        string subId = item.Name.textId;
+                        var dtypeId = item.Item is DatatypeRefT refT ? refT.datatypeId : null;
+                        if (dtypeId != null && datatypeMap.TryGetValue(dtypeId, out var nestedType))
+                        {
+                            ParseDatatype(nestedType, datatypeMap, variables, subId, index, 
+                                item.subindex, item.bitOffset, isDynamic, access, null);
+                        }
+                    }
+                    return; // no direct variable to add here
+            }
+
+            var variable = new Variable(
+                name: parentId,
+                index: index,
+                subindex: subindex,
+                access: access,
+                isDynamic: isDynamic,
+                dataType: dataType,
+                arrayCount: 1,
+                lengthInBits: lengthInBits,
+                offset: offset,
+                defaultValue: defaultValue,
+                minimum: minValue,
+                maximum: maxValue,
+                valid: valid,
+                value: null
+            );
+
+            variables.Add(variable);
         }
 
         public static (string? Min, string? Max) GetMinMaxFromValueRange(AbstractValueT[]? valueRange)
@@ -165,13 +187,11 @@ namespace DeviceDescriptor.IoLink.Source
             if (valueRange == null)
                 return (null, null);
 
-            return ((AbstractValueT[])valueRange)[0] switch
+            return valueRange.FirstOrDefault() switch
             {
                 UIntegerValueRangeT u => (u.lowerValue.ToString(), u.upperValue.ToString()),
                 IntegerValueRangeT i => (i.lowerValue.ToString(), i.upperValue.ToString()),
                 Float32ValueRangeT f => (f.lowerValue.ToString(), f.upperValue.ToString()),
-
-                // Add other range types here if needed
                 _ => (null, null)
             };
         }
@@ -191,6 +211,6 @@ namespace DeviceDescriptor.IoLink.Source
             }).Where(s => s != null);
 
             return string.Join(",", values!);
-        }    
+        }
     }
 }
